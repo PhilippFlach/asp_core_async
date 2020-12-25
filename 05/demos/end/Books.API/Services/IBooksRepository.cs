55a7a9129f3af8ee324dc75aafd2f05d7ac7eee1
Philipp Flach <flach.philipp@luzi.ch>
2020-12-25 10:25:54 +0100

    Add project files.

---
+++ 05/demos/end/Books.API/Services/IBooksRepository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Books.API.Services
+{
+    public interface IBooksRepository
+    {
+        IEnumerable<Entities.Book> GetBooks();
+
+        //Entities.Book GetBook(Guid id);
+
+        Task<IEnumerable<Entities.Book>> GetBooksAsync();
+
+        Task<Entities.Book> GetBookAsync(Guid id);
+
+        Task<IEnumerable<Entities.Book>> GetBooksAsync(IEnumerable<Guid> bookIds);
+
+        void AddBook(Entities.Book bookToAdd);
+
+        Task<bool> SaveChangesAsync();
+    }
+}
